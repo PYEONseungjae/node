90cf17587af4da749289ae08dcc5e196150f4ed8
PYEONseungjae <jss_810@naver.com>
2021-11-15 19:35:06 +0900

    에러2

--- model/db.js
+++ model/db.js
@@ -8,7 +8,7 @@ sequelize = new Sequelize("class101","root","djflsdl1@",{
     dialect : "mysql",//시퀄라이즈는 몽고 등 다른 db도 지원하기 때문에 mysql를 사용하겠다고 지정
     timezone : "+09:00",
     define : {
-        Character : "utf8",
+        charset : "utf8",
         collate : "utf8_general_ci",
         timestamps : true,
         freezeTableName : true
@@ -16,7 +16,7 @@ sequelize = new Sequelize("class101","root","djflsdl1@",{
 })
 
 var db = {};
-db.users = sequelize.import(__dirname + "/users.js");
+//db.users = sequelize.import(__dirname + "/users.js");
 //만들어낸 테이블은 시퀄라이즈 임폴트로 가지고 와야함.
 //__dirname 은 상대주소를 의미함. 해당 코드 바로 근처부터 찾는다는 의미
 
